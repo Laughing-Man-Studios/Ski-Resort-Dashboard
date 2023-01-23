f87b2502527c47ddfab2c337dc8089d06e3da917
Rogibb111 <Rogibb111@users.noreply.github.com>
2023-01-23 09:57:22 -0700

    Adding static navbar mobile elements and styles

--- public/javascripts/Navbar.js
+++ public/javascripts/Navbar.js
@@ -1,28 +1,54 @@
-import {LitElement, css, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+import {LitElement, css, html, classMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
 export class Navbar extends LitElement {
   static properties = {
-    name: {},
+    _linksContainerClasses: { state: true },
+    imageSrc: { type: String }
   };
   // Define scoped styles right with your component, in plain CSS
   static styles = css`
-    :host {
-      color: blue;
+    #menu-button {
+      margin: 0.5rem;
+    }
+    
+    #menu-button div {
+      width: 1.5rem;
+      height: 0.2rem;
+      background-color: white;
+      margin: 0.3rem 0;
+    }
+    .hidden {
+      display: none;
+    }
+
+    #nav-container {
+      display: flex;
+      justify-content: space-between;
+      background-color: #4388cc;
     }
   `;
 
   constructor() {
     super();
     // Declare reactive properties
-    this.isOpen = false;
+    this._linksContainerClasses = { hidden: true }
+    this.imageSrc = '';
   }
 
   // Render the UI as a function of component state
   render() {
     return html`
-      <div>
-        <slot name=""></slot>
+      <div id="nav-container">
+        <img src=${this.imageSrc} title="pageIcon"/>
+        <div id="menu-button" >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
+      <div id="links-container" class=${classMap(this._linksContainerClasses)}>
+          <slot name=""></slot>
+        </div>
     `;
   }
 }
